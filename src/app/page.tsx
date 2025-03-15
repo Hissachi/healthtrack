f0b032798de4467d06247ca0a11845da40b68538
Hissachi <lucashissachi@gmail.com>
2025-03-15 13:03:47 -0300

    duas colunas adicionadas e melhoria do visual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,9 @@ export default function Home() {
           Monitoramento de Saúde
         </div>
       </header>
-      <main className="grid grid-cols-2 gap-[32px] row-start-2 items-center sm:items-start bg-white m-10 p-8 border-2 border-gray-200 rounded-2xl shadow-md">
-        <div className="absolute inset-0 pb-6.5 flex items-center justify-center opacity-5 font-light">|</div>
-        <div className="absolute inset-0 flex items-center justify-center opacity-10 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-6.5 flex items-center justify-center opacity-15 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-13 flex items-center justify-center opacity-20 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-19.5 flex items-center justify-center opacity-25 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-26 flex items-center justify-center opacity-30 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-32.5 flex items-center justify-center opacity-35 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-39 flex items-center justify-center opacity-30 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-45.5 flex items-center justify-center opacity-25 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-52 flex items-center justify-center opacity-20 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-58.5 flex items-center justify-center opacity-15 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-65 flex items-center justify-center opacity-10 font-light z-0">|</div>
-        <div className="absolute inset-0 pt-71.5 flex items-center justify-center opacity-5 font-light z-0">|</div>
+      <main className="grid grid-cols-4 gap-[32px] row-start-2 items-center sm:items-start bg-white m-10 p-8 border-2 border-gray-200 rounded-2xl shadow-md">
         <form className="flex flex-col z-1">
-          <div className="flex justify-center font-bold">DADOS PESSOAIS</div>
+          <div className="flex justify-center font-bold border-1 rounded-md border-indigo-200">DADOS PESSOAIS</div>
           <div className="flex justify-center pb-3 font-extralight transform opacity-20">------------------------------</div>
           <input placeholder="Nome Completo" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="text"></input>
           <input placeholder="Idade" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
@@ -41,7 +28,7 @@ export default function Home() {
           <input placeholder="Peso (em g)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
         </form>
         <form className="flex flex-col z-1">
-          <div className="flex justify-center font-bold">DADOS DE ATIVIDADE</div>
+          <div className="flex justify-center font-bold border-1 rounded-md border-red-200">ATIVIDADE</div>
           <div className="flex justify-center pb-3 font-extralight transform opacity-20">------------------------------</div>
           <select className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="treino">
             <option defaultValue=""disabled selected>Selecione o treino</option>
@@ -62,14 +49,45 @@ export default function Home() {
           <input placeholder="Duração (em minutos)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
           <select className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="intensidade">
             <option defaultValue=""disabled selected>Intensidade do treino</option>
-            <option value="treino-1">Leve</option>
-            <option value="treino-2">Moderada</option>
-            <option value="treino-3">Forte</option>
+            <option value="intensidade-1">Leve</option>
+            <option value="intensidade-2">Moderada</option>
+            <option value="intensidade-3">Forte</option>
           </select>
           <input placeholder="Gasto Calórico (em kJ)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
         </form>
-        <div className="col-span-2 flex justify-center items-center">
-          <button className="border-3 border-gray-400 bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-700 hover:to-blue-700 transition ease-in-out duration-500 cursor-pointer shadow-md shadow-indigo-600 hover:shadow-black" type="submit">
+        <form className="flex flex-col z-1">
+          <div className="flex justify-center font-bold border-1 rounded-md border-yellow-200">ALIMENTAÇÃO</div>
+          <div className="flex justify-center pb-3 font-extralight transform opacity-20">------------------------------</div>
+          <input placeholder="Número de refeições por dia" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+          <input placeholder="Consumo de água diário (ml)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+          <select className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="suplemento">
+            <option defaultValue=""disabled selected>Utiliza algum suplemento?</option>
+            <option value="suplemento-1">Sim</option>
+            <option value="suplemento-2">Não</option>
+          </select>
+          <select className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="nutricionista">
+            <option defaultValue=""disabled selected>Frequenta nutricionista?</option>
+            <option value="nutricionista-1">Sim</option>
+            <option value="nutricionista-2">Não</option>
+          </select>
+          <input placeholder="Consumo Calórico (em kJ)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+        </form>
+        <form className="flex flex-col z-1">
+          <div className="flex justify-center font-bold border-1 rounded-md border-purple-200">SAÚDE GERAL</div>
+          <div className="flex justify-center pb-3 font-extralight transform opacity-20">------------------------------</div>
+          <input placeholder="Tempo de sono diário (horas)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+          <select className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="sono">
+            <option defaultValue=""disabled selected>Qualidade do sono</option>
+            <option value="sono-1">Boa</option>
+            <option value="sono-2">Moderada</option>
+            <option value="sono-3">Ruim</option>
+          </select>
+          <input placeholder="Nível de estresse (1-10)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+          <input placeholder="Pressão arterial (mmHg)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+          <input placeholder="Frequência cardíaca (bpm)" className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="number"></input>
+        </form>
+        <div className="col-span-4 flex justify-center items-center">
+          <button className="border-3 w-100 border-gray-400 bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-700 hover:to-blue-700 transition ease-in-out duration-500 cursor-pointer shadow-md shadow-indigo-600 hover:shadow-black z-1" type="submit">
             Enviar
           </button>
         </div>
