7d6cdfdc77b743d563cbf028c26e65453dd8fca2
Hissachi <lucashissachi@gmail.com>
2025-03-17 10:58:31 -0300

    Retirada do mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-export default function Home() {
+export default async function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-gray-100 to-blue-200">
       <header className="row-start-1 flex gap-[24px] items-center justify-center">
